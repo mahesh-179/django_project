28a0358ab51937b6c6cedf1a4aee25118b99cc58
Mahesh Raj Lamsal <maheshrajlamsal01@gmail.com>
2026-02-13 23:29:28 +0545

    Django Inbuild Validators

--- firstapp/forms.py
+++ firstapp/forms.py
@@ -1,19 +1,20 @@
 from django import forms
+from django.core import validators
 class StudentForm(forms.Form):
-    ename=forms.CharField()
+    ename=forms.CharField(validators=[validators.MinLengthValidator(5)])
     add=forms.CharField()
-    age=forms.IntegerField()
+    age=forms.IntegerField(validators=[validators.MinValueValidator(18)])
     gpa=forms.FloatField()
     college=forms.CharField()
 
     
-    def clean_ename(self):
-        inputname=self.cleaned_data["ename"]
-        if len(inputname)<5:
-            raise forms.ValidationError("Name is too short please enter long name")
-        return inputname
-    def clean_age(self):
-        inputage=self.cleaned_data["age"]
-        if (inputage)<=18:
-            raise forms.ValidationError("Age Should Be Greater or Equal than 18 ")
-        return inputage
+    # def clean_ename(self):
+    #     inputname=self.cleaned_data["ename"]
+    #     if len(inputname)<5:
+    #         raise forms.ValidationError("Name is too short please enter long name")
+    #     return inputname
+    # def clean_age(self):
+    #     inputage=self.cleaned_data["age"]
+    #     if (inputage)<=18:
+    #         raise forms.ValidationError("Age Should Be Greater or Equal than 18 ")
+    #     return inputage
